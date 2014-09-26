01c5bd933858d1dc9b4f9468a2f7d8cfa26f83e8
user <user>
2014-09-26 19:38:24 +1000

    lang fix

--- setup.sh
+++ setup.sh
@@ -4,13 +4,6 @@
 echo "Europe/Berlin" | sudo tee /etc/timezone
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 
-# console setup
-echo "locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8, ru_RU.UTF-8 UTF-8" | debconf-set-selections
-echo "locales locales/default_environment_locale select ru_RU.UTF-8" | debconf-set-selections
-sed -i 's/^# ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
-dpkg-reconfigure --frontend=noninteractive locales
-export LANG=ru_RU.UTF-8
-
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
